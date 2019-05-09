d2e20b960cc92d3b5471efd2495ad2abcd58e592
Filip Strnad <strnadf@fzp.czu.cz>
2019-05-09 07:30:53 +0000

    version 1.0.1

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+[![Travis build status](https://travis-ci.org/strnda/CoSMoS.svg?branch=master)](https://travis-ci.org/strnda/CoSMoS)
+[![AppVeyor build status](https://ci.appveyor.com/api/projects/status/github/strnda/CoSMoS?branch=master&svg=true)](https://ci.appveyor.com/project/strnda/CoSMoS)
+[![CRAN_Release_Badge](http://www.r-pkg.org/badges/version-ago/CoSMoS)](https://CRAN.R-project.org/package=CoSMoS)
+[![CRAN_Download_Badge](http://cranlogs.r-pkg.org/badges/grand-total/CoSMoS)](https://CRAN.R-project.org/package=CoSMoS)
+[![license](https://img.shields.io/badge/license-GPL3-lightgrey.svg)](https://choosealicense.com/)
+
+
 # CoSMoS - Complete Stochastic Modelling Solution
 CoSMoS is an R package that makes time series generation with desired properties easy. Just choose the characteristics of the time series you want to generate, and it will do the rest.
 The generated time series preserve any probability distribution and any linear autocorrelation structure. Users can generate as many and as long time series from processes such as precipitation, wind, temperature, relative humidity etc. It is based on a framework that unified, extended, and improved a modelling strategy that generates time series by transforming “parent” Gaussian time series having specific characteristics (Papalexiou, 2018).
@@ -6,11 +13,15 @@ The generated time series preserve any probability distribution and any linear a
 To install the latest version of the package run:
 
 ```r
+## copy-paste to get the latest version of CoSMoS
+
 if (!require('devtools')) {install.packages('devtools'); library(devtools)} 
 
 install_github('strnda/CoSMoS', upgrade = 'never')
 
 library(CoSMoS)
+
+?`CoSMoS-package`
 ```
 
 # Funding
@@ -18,8 +29,8 @@ The package was partly funded by the Global institute for Water Security (GIWS; 
 
 # Authors
 Coded and maintained by: Filip Strnad    
-Conceptual design by: Simon Michael Papalexiou, and Filip Strnad    
-Beta tested by: Filip Strnad, Yannis Markonis, and Simon Michael Papalexiou    
+Conceptual design by: Simon Michael Papalexiou     
+Tested and documented by: Yannis Markonis     
 
 # References
 Papalexiou, S.M., 2018. Unified theory for stochastic modelling of hydroclimatic processes: Preserving marginal distributions, correlation structures, and intermittency. Advances in Water Resources 115, 234-252. https://doi.org/10.1016/j.advwatres.2018.02.013
--- inst/doc/vignette.R
+++ inst/doc/vignette.R
@@ -1,69 +1,184 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE, fig.width = 7, eval = FALSE)
+
 ## ---- message = FALSE----------------------------------------------------
-library(CoSMoS)
-
-marginaldist <- 'burrXII'
-para <- list(scale = 2,
-             shape1 = .75,
-             shape2 = .1)
+#  library(CoSMoS)
 
 ## ------------------------------------------------------------------------
-p0 <- .9
+#  marginaldist <- 'ggamma'
+#  param <- list(scale = 1,
+#                shape1 = .8,
+#                shape2 = .8)
 
 ## ------------------------------------------------------------------------
-order <- 100
-targetcorr <- acfparetoII(t = 0:order, scale = 15, shape = .3)
+#  no <- 1000
+#  ggamma_sim <- rggamma(n = no,
+#                        scale = 1,
+#                        shape1 = 1,
+#                        shape2 = .5)
+#  plot.ts(ggamma_sim, main = "")
+#  par(mfrow = c(1, 2))
+#  plot(density(ggamma_sim), main = "")
+#  acf(ggamma_sim, main = "")
 
 ## ------------------------------------------------------------------------
-p <- actpnts(marginaldist, para, p0 = p0)
+#  acf <- c(1, 0.8)
+#  ggamma_sim <- generateTS(n = no,
+#                           margdist = marginaldist,
+#                           margarg = param,
+#                           acsvalue = acf)
+#  plot(ggamma_sim, main = "")
+#  par(mfrow = c(1, 2))
+#  plot(density(ggamma_sim[[1]]), main = "")
+#  acf(ggamma_sim[[1]], main = "")
 
 ## ------------------------------------------------------------------------
-f <- fitactf(p)
-plot(f)
+#  acf <- c(1, 0.5, 0.5, 0.4, 0.4) #up to lag-4
+#  ggamma_sim <- generateTS(n = no,
+#                           margdist = marginaldist,
+#                           margarg = param,
+#                           acsvalue = acf)
+#  plot(ggamma_sim, main = "")
+#  par(mfrow = c(1, 2))
+#  plot(density(ggamma_sim[[1]]), main = "")
+#  acf(ggamma_sim[[1]], main = "")
 
 ## ------------------------------------------------------------------------
-target <- ARp(margdist = marginaldist,
-              margarg = para,
-              actfpara = f,
-              n = 100000,
-              p0 = p0,
-              p = order,
-              acsvalue = targetcorr)
+#  ## specify lag
+#  lags <- 0:10
+#  
+#  ## get the ACS
+#  f <- acs('fgn', t = lags, H = .75)
+#  b <- acs('burrXII', t = lags, scale = 1, shape1 = .6, shape2 = .4)
+#  w <- acs('weibull', t = lags, scale = 2, shape = 0.8)
+#  p <- acs('paretoII', t = lags, scale = 3, shape = 0.3)
+#  
+#  ## visualize the ACS
+#  dta <- data.frame(lags, f, b, w, p)
+#  
+#  m.dta <- melt(dta, id.vars = 'lags')
+#  
+#  ggplot(m.dta,
+#         aes(x = lags,
+#             y = value,
+#             group = variable,
+#             colour = variable)) +
+#    geom_point(size = 2.5) +
+#    geom_line(lwd = 1) +
+#    scale_color_manual(values = c('steelblue4', 'red4', 'green4', 'darkorange'),
+#                       labels = c('FGN', 'Burr XII', 'Weibull', 'Pareto II'),
+#                       name = '') +
+#    labs(x = bquote(lag ~ tau),
+#         y = 'Acf') +
+#    scale_x_continuous(breaks = t) +
+#    theme_grey()
 
-## ---- fig.show = 'hold'--------------------------------------------------
-gauss <- attr(target, 'gaussian')
+## ------------------------------------------------------------------------
+#  acf = acs(id = 'paretoII',
+#            t = 0:30,
+#            scale = 1,
+#            shape = .75)
+#  
+#  ggamma_sim <- generateTS(n = no,
+#                           margdist = marginaldist,
+#                           margarg = param,
+#                           acsvalue = acf)
+#  par(mfrow = c(1, 1))
+#  plot(ggamma_sim, main = "")
 
-ggplot() +
-  geom_point(aes(x = 0:order,
-                 y = as.vector(acf(gauss, lag.max = order, plot = F)$acf)), colour = 'red2', alpha = .2) +
-  geom_line(aes(x = 0:order,
-                y = attr(target, 'trACS')), colour = 'red4') +
-  theme_bw() +
-  labs(x = 'Lag',
-       y = 'ACF',
-       title = 'Gaussian ACS')
+## ------------------------------------------------------------------------
+#  my_acf <- exp(seq(0, -2, -0.1))
+#  ggamma_sim <- generateTS(n = no,
+#                           margdist = marginaldist,
+#                           margarg = param,
+#                           acsvalue = my_acf)
+#  par(mfrow = c(1, 1))
+#  plot(ggamma_sim, main = "")
+#  par(mfrow = c(1, 2))
+#  acf(ggamma_sim[[1]])
 
-ggplot() +
-  geom_point(aes(x = 0:order,
-                 y = as.vector(acf(target, lag.max = order, plot = F)$acf)), colour = 'steelblue2', alpha = .2) +
-  geom_line(aes(x = 0:order,
-                y = targetcorr), colour = 'steelblue4') +
-  theme_bw() +
-  labs(x = 'Lag',
-       y = 'ACF',
-       title = 'Target ACS')
+## ---- fig.height = 7-----------------------------------------------------
+#  prob_zero <- .9
+#  ggamma_sim <- generateTS(n = no,
+#                           margdist = marginaldist,
+#                           margarg = param,
+#                           acsvalue = acf,
+#                           p0 = prob_zero,
+#                           TSn = 5)
+#  par(mfrow = c(1, 1))
+#  plot(ggamma_sim, main = "")
 
-## ---- comment = '>'------------------------------------------------------
-checksimulation(target)
+## ------------------------------------------------------------------------
+#  checkTS(ggamma_sim)
 
-## ---- out.width = '95%', fig.asp = .75, results = 'asis'-----------------
-chck <- checkmodel(margdist = marginaldist,
-                   margarg = para,
-                   n = 100000,
-                   p = order,
-                   p0 = p0,
-                   acsvalue = targetcorr,
-                   TSn = 15,
-                   plot = T)
+## ------------------------------------------------------------------------
+#  data('precip')
+#  plot(precip, type = 'l')
+#  par(mfrow = c(1, 2))
+#  plot(density(precip$value), main = "", xlim = c(0, 10)) #Does not plot extreme values
+#  acf(precip$value, main = "", lag.max = 20)
 
-knitr::kable(chck)
+## ---- fig.height = 9-----------------------------------------------------
+#  precip_ggamma <- analyzeTS(TS = precip,
+#                                     season = 'month',
+#                                     dist = 'ggamma',
+#                                     acsID = "weibull",
+#                                     lag.max = 12)
+#  reportTS(precip_ggamma, 'dist')
+#  reportTS(precip_ggamma, 'acs')
+#  reportTS(precip_ggamma, 'stat')
+
+## ---- warning = FALSE, fig.height = 9------------------------------------
+#  precip_pareto <- analyzeTS(TS = precip,
+#                                     season = 'month',
+#                                     dist = 'paretoII',
+#                                     acsID = 'fgn',
+#                                     lag.max = 12)
+#  reportTS(precip_pareto, 'dist')
+#  reportTS(precip_pareto, 'acs')
+
+## ------------------------------------------------------------------------
+#  sim_precip <- simulateTS(aTS = precip_ggamma,
+#                           from = as.POSIXct('1978-12-01 00:00:00'),
+#                           to = as.POSIXct('2008-12-01 00:00:00'))
+#  dta <- precip
+#  dta[, id := 'observed']
+#  sim_precip[, id := 'simulated']
+#  
+#  dta <- rbind(dta, sim_precip)
+#  
+#  ggplot(dta) +
+#    geom_line(aes(x = date, y = value)) +
+#    facet_wrap(~id, ncol = 1) +
+#    theme_classic()
+
+## ------------------------------------------------------------------------
+#  id <- '02135000'
+#  dta_raw <- as.data.table(read.fwf(sprintf(
+#    'ftp://hydrology.nws.noaa.gov/pub/gcip/mopex/US_Data/Us_438_Daily/%s.dly', id),
+#                                    widths = c(8,10,10,10,10,10),
+#                                    col.names = c('date', 'P', 'E', 'value', 'Tmax', 'Tmin')))
+#  
+#  dta_raw[, date := as.POSIXct(gsub(' ','0', date), format = '%Y%m%d')]
+#  
+#  daily_streamflow <- dta_raw[value >= 0, .(date, value)]
+#  
+#  str_burr <- analyzeTS(daily_streamflow,
+#                        dist = 'ggamma',
+#                        norm = 'N4',
+#                        acsID = 'paretoII',
+#                        lag.max = 50)
+#  
+#  sim_str <- simulateTS(str_burr)
+#  
+#  dta <- daily_streamflow
+#  dta[, id := 'observed']
+#  sim_str[, id := 'simulated']
+#  
+#  dta <- rbind(dta, sim_str)
+#  
+#  ggplot(dta) +
+#    geom_line(aes(x = date, y = value)) +
+#    facet_wrap(~id, ncol = 1) +
+#    theme_classic()
 
